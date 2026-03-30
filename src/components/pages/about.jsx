cd287f24cc0164e2308635e1f8f79e1b45ea8b8f
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2026-03-30 08:31:03 +0530

    Feat: Added editor configurations, updated /about

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -24,15 +24,38 @@ const About = () => {
       <Card title={header}>
         <p>
           Rent Back is a shopping experience for the new generation. Everytime
-          you need a thing like a ladder, a drill or maybe a book yk you are not
-          gonna use again, or maybe you really want a guitar after you came to a
-          new locality for vacations? RentBack got you covered.
+          you need a thing like a ladder, a drill or maybe a book you know you
+          are not going to use again, or maybe you really want a guitar after
+          you came to a new locality for vacations? RentBack got you covered.
         </p>
         <p>
           It is a revolutionary idea that utilises the usual economy of
           borrowing, selling, reselling or maybe simple "I dont need it
           anymore". It is useful, because people can put items for rent, so that
           people can keep utilising the same object.
+        </p>
+        <p>
+          Also, together we can enhance the idea of reduce and reuse, and save
+          the planet Earth.
+        </p>
+      </Card>{" "}
+      <br />
+      <Card title={<h3>Meet the Creator</h3>}>
+        <p>
+          Pritam Santra (
+          <span className="italic underline-offset-2 underline hover:animate-bounce">
+            <a href="https://www.github.com/cameraman-pritam">
+              cameraman-pritam
+            </a>
+          </span>{" "}
+          on github,{" "}
+          <span className="italic underline-offset-2 underline hover:animate-bounce">
+            <a href="https://www.instagram.com/cameraman.pritam">
+              @cameraman.pritam
+            </a>
+          </span>{" "}
+          on Instagram) is a tech newby, who really wanted to create something,
+          so came up to this.
         </p>
       </Card>
     </>
