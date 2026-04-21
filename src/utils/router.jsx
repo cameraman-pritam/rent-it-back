6772d3ddb729d6fda43e0359107146dc7def334a
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2026-04-21 15:42:16 +0530

    perf: Updated FS and configs

---
+++ src/utils/router.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "../pages/about";
+import Layout from "../layout/Layout";
+import Home from "../pages/home.jsx";
+import Browse from "../pages/browse";
+import Contribute from "../pages/contribute";
+import NotFound from "../pages/notfound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // This is where your Navbar lives
+    children: [
+      { index: true, element: <Home /> },
+      { path: "browse", element: <Browse /> },
+      { path: "about", element: <About /> },
+      { path: "contribute", element: <Contribute /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
