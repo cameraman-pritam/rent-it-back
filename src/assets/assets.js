73f5481da83e397c8353985327dffd937de1b03f
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2026-04-22 12:45:49 +0530

    Chore: Unified Imports

---
+++ src/assets/assets.js
@@ -0,0 +1,6 @@
+import exchange from "./exchange.png";
+import hero from "./hero.png";
+import logoNavbar from "./logo-navbar.png";
+import logo from "./logo.png";
+
+export { exchange, hero, logoNavbar, logo };
